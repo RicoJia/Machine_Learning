a29a943adfb993ae433aaf51aac913a6e4180848
Rico <ricoruotongjia@gmail.com>
2024-10-08 16:20:34 -0500

    run rico neuralnet

--- run_rico_neuralnet_prototype.sh
+++ run_rico_neuralnet_prototype.sh
@@ -1,4 +1,12 @@
 # jetson-containers build --name=rico_cuda_image jupyterlab pytorch
+show_help(){
+  echo "Usage: $0 [-j] [-h]"
+  echo
+  echo "Options:"
+  echo "  -j    Enable Jupyter"
+  echo "  -h    Show help"
+  exit 0
+}
 
 echo "Preparing Xauthority data..."
 xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
@@ -29,26 +37,66 @@ THIS_DIRECTORY_PATH="$(dirname "${BASH_SOURCE[0]}")"
 THIS_DIRECTORY_NAME="$(basename "$(dirname "${BASH_SOURCE[0]}")")"
 # Add this for nvidia 
 # --runtime=nvidia \
-docker run \
-    --name ${IMAGE_NAME}-container \
-    -e DISPLAY=$DISPLAY \
-    -e QT_X11_NO_MITSHM=1 \
-    -e XAUTHORITY=$XAUTH \
-    -v /etc/passwd:/etc/passwd:ro \
-    -v /etc/group:/etc/group:ro \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v $HOME/.Xauthority:/root/.Xauthority \
-    -v ${THIS_DIRECTORY_PATH}:/home/${USER}/${THIS_DIRECTORY_NAME} \
-    -v ~/.ssh:/root/.ssh \
-    -v /dev/bus/usb:/dev/bus/usb \
-    -v $XAUTH:$XAUTH \
-    -v /dev:/dev:rw \
-    -p 8888:8888 \
-    -it --rm \
-    --runtime nvidia \
-    --network=host \
-    --privileged \
-    ${IMAGE_NAME}:${TAG_NAME} \
-    jupyter notebook --ip=0.0.0.0 --no-browser --allow-root
+
+jupyter_enabled=false
+while getopts "jh" opt; do
+  case ${opt} in
+    j)
+      jupyter_enabled=true
+      ;;
+    h)
+      show_help
+      ;;
+    *)
+      show_help
+      ;;
+  esac
+done
+
+if $jupyter_enabled; then
+echo "jupyter"
+    docker run \
+        --name ${IMAGE_NAME}-container \
+        -e DISPLAY=$DISPLAY \
+        -e QT_X11_NO_MITSHM=1 \
+        -e XAUTHORITY=$XAUTH \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v /etc/group:/etc/group:ro \
+        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        -v $HOME/.Xauthority:/root/.Xauthority \
+        -v ${THIS_DIRECTORY_PATH}:/home/${USER}/${THIS_DIRECTORY_NAME} \
+        -v ~/.ssh:/root/.ssh \
+        -v /dev/bus/usb:/dev/bus/usb \
+        -v $XAUTH:$XAUTH \
+        -v /dev:/dev:rw \
+        -p 8888:8888 \
+        -it --rm \
+        --runtime nvidia \
+        --network=host \
+        --privileged \
+        ${IMAGE_NAME}:${TAG_NAME} \
+        jupyter notebook --ip=0.0.0.0 --no-browser --allow-root
+else 
+    docker run \
+        --name ${IMAGE_NAME}-container \
+        -e DISPLAY=$DISPLAY \
+        -e QT_X11_NO_MITSHM=1 \
+        -e XAUTHORITY=$XAUTH \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v /etc/group:/etc/group:ro \
+        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        -v $HOME/.Xauthority:/root/.Xauthority \
+        -v ${THIS_DIRECTORY_PATH}:/home/${USER}/${THIS_DIRECTORY_NAME} \
+        -v ~/.ssh:/root/.ssh \
+        -v /dev/bus/usb:/dev/bus/usb \
+        -v $XAUTH:$XAUTH \
+        -v /dev:/dev:rw \
+        -p 8888:8888 \
+        -it --rm \
+        --runtime nvidia \
+        --network=host \
+        --privileged \
+        ${IMAGE_NAME}:${TAG_NAME}
+fi
 
  #docker run --runtime nvidia -it --rm --network=host -v /home/rico:/home/rico rico_cuda_image:r36.3.0-cu122-torchvision
