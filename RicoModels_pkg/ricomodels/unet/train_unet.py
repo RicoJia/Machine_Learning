e4d9a956247eb10b8e33bacf41aef92b1c435452
rico-orin-nano <ricoruotongjia@gmail.com>
2024-10-16 22:15:50 -0500

    more

--- RicoModels_pkg/ricomodels/unet/train_unet.py
+++ RicoModels_pkg/ricomodels/unet/train_unet.py
@@ -15,11 +15,11 @@ import functools
 BATCH_SIZE = 8
 MODEL_PATH = os.path.join(get_pkg_dir(), "unet/unet_pascal.pth")
 CHECKPOINT_DIR = os.path.join(get_pkg_dir(), "unet/checkpoints")
-ACCUMULATION_STEPS = 32/BATCH_SIZE
+ACCUMULATION_STEPS = int(32/BATCH_SIZE)
 NUM_EPOCHS=70
 LEARNING_RATE=1e-5
 SAVE_CHECKPOINT = True
-SAVE_EVERY_N_EPOCH = 10
+SAVE_EVERY_N_EPOCH = 5
 AMP = False
 INTERMEDIATE_BEFORE_MAX_POOL = False
 WEIGHT_DECAY = 1e-8
@@ -33,34 +33,6 @@ def _check_channel(model_channels, img_channels):
             f'but loaded images have {img_channels} channels. Please check that ' \
             'the images are loaded correctly.'
         )
-
-def eval_model(model, test_loader, device, visualize: bool = False):
-    # Evaluation phase
-    model.eval()
-    correct_test = 0
-    total_test = 0
-    with torch.no_grad():
-        # TODO I AM ITERATING OVER TRAIN_LOADER, SO I'M MORE SURE
-        for inputs_test, labels_test in test_loader:
-            inputs_test = inputs_test.to(device)
-            labels_test = labels_test.to(device)
-            outputs_test = model(inputs_test)
-            _, predicted_test = outputs_test.max(1)
-            mask = (labels_test != 21)
-            local_total = mask.sum().item()
-            local_correct = (predicted_test.eq(labels_test) & mask).sum().item()
-            total_test += local_total
-            correct_test += local_correct
-
-            if visualize:
-                #TODO Remember to remove
-                print(f'Rico: predicted test acc {100. * local_correct/local_total}%')
-                for img, pred, lab in zip(inputs_test, predicted_test, labels_test):
-                    print("pred uniq: ", torch.unique(pred), "lab uniq: ", torch.unique(lab))
-                    visualize_image_and_target(img.cpu(), pred.cpu(), lab.cpu())
-
-    test_acc = 100. * correct_test / total_test
-    print(f'Test Acc: {test_acc:.2f}%')
 
 def validate_model(model, val_loader, device):
     model.eval()
@@ -82,7 +54,7 @@ def train_model(model, train_loader, val_loader,
         # Training phase
         start = time.time()
         model.train()
-        running_loss = 0.0
+        epoch_loss = 0.0
         correct_train = 0
         total_train = 0
         
@@ -101,26 +73,25 @@ def train_model(model, train_loader, val_loader,
                 pbar.update(inputs.shape[0])
                 pbar.set_postfix(**{'loss (batch)': loss.item()})
 
-            # Backward pass and optimization
-            loss.backward()
-            if (i+1)%ACCUMULATION_STEPS == 0:
-                optimizer.step()
-                optimizer.zero_grad()
-    #         # Statistics
-    #         running_loss += loss.item() * inputs.size(0)
+                loss.backward()
+                if (i+1)%ACCUMULATION_STEPS == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
+                epoch_loss += loss.item() 
+
+            epoch_loss /= num_training
     #         _, predicted = outputs.max(1)
     #         # print(predicted.shape)
     #         total_train += mask.sum().item()
     #         # print((predicted == labels).sum().item(), ((predicted == labels) & mask).sum().item())
     #         correct_train += ((predicted == labels) & mask).sum().item()
     #     epoch_acc = 100. * correct_train / total_train
-    #     print("correct train: ", correct_train, " total train: ", total_train)
             
             current_lr = optimizer.param_groups[0]['lr']
             validation_loss = validate_model(model, val_loader, device)
             scheduler.step(metrics=validation_loss)  # TODO: disabled for Adam optimizer
             wandb_logger.log({
-                'train loss': loss.item(),
+                'epoch loss': epoch_loss,
                 'epoch': epoch,
                 'learning rate': current_lr
             })
@@ -145,14 +116,6 @@ if __name__ == "__main__":
         num_workers = 2,
         pin_memory = True
     ) 
-    val_dataset = VOCSegmentationClass(image_set='val', year='2012')
-    val_dataloader = torch.utils.data.DataLoader(
-        val_dataset,
-        batch_size = BATCH_SIZE,
-        shuffle=False,  # A bit more deterministic here
-        num_workers = 2,
-        pin_memory = True
-    )
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     class_num = len(train_dataset.classes)
--- RicoModels_pkg/ricomodels/utils/visualization.py
+++ RicoModels_pkg/ricomodels/utils/visualization.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import os
+import time
+
+RESULTS_DIR = "/tmp/results/"
 
 def visualize_image_target_mask(image, target=None, labels=None):
     # # See torch.Size([3, 281, 500]) torch.Size([1, 281, 500])
@@ -26,4 +30,9 @@ def visualize_image_target_mask(image, target=None, labels=None):
     # See tensor([  0,   1,  15, 255], dtype=torch.uint8)
     plt.show()
     tiempo = int(time.time() * 1000)
-    plt.savefig(str(tiempo)+".png")
+
+
+    if not os.path.exists(RESULTS_DIR):
+        os.mkdir(RESULTS_DIR)
+
+    plt.savefig(RESULTS_DIR + str(tiempo)+".png")
